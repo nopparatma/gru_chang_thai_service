e77435b77cb1db26e5cf14b031c7f02338589a59
Nopparat Manoi <yachty22541@gmail.com>
2024-11-16 03:07:47 +0700

    remove unused

--- app.js
+++ app.js
@@ -1,54 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require('dotenv');
-const connectDB = require("./db");
-const cors = require('cors');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user_controller');
-var productRouter = require('./routes/product_controller');
-var masterRouter = require('./routes/master_controller');
-
-var app = express();
-app.use(cors());
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes";
+import masterRoutes from "./routes/masterRoutes";
 
 dotenv.config();
 
-console.log(`Database Connecting...`);
-connectDB();
+const app = express();
+const port = process.env.SERVER_PORT;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/master', masterRouter);
-app.use('/product', productRouter);
-app.use('/user', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// Health Check Endpoint
+app.get("/", (req, res) => {
+  res.send("gru_chang_thai_service is running");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// routes
+app.use("/v1/product", productRoutes);
+app.use("/v1/master", masterRoutes);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI ?? "")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running at PORT : ${port}`);
 });
-
-module.exports = app;
